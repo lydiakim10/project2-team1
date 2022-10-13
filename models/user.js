e85572fb958f031e474f421861e62ab5b8308bce
Alan Huang <a.huang232@gmail.com>
2022-10-12 20:40:09 -0700

    Add boiler plate code to server.js / connection.js & create user model / index.js

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    // Check that the entered password matches password in the database
+    checkPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+
+    // Encrypt user's password before adding to database
+    {
+        hooks: {
+        beforeCreate: async (newUserData) => {
+
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+        beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
