560c55459b6c420b2d96de7777c9974c6170d781
Dylan Yamashiro <dylanyamashiro@gmail.com>
2022-10-13 20:50:28 -0700

    added records

---
+++ models/category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Category extends Model {}
+
+Category.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    budget: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            isFloat: true,
+        }
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Category',
+});
+
+module.exports = Category;
---
+++ models/records.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Records extends Model {}
+
+Records.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: category,
+            key: id,
+        }
+    },
+    cost: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            isFloat: true,
+        }
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+            isDate: true,
+        }
+    },
+    merchant: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'records',
+});
+
+module.exports = Records;
