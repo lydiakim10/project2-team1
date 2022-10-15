6576b7be7ae0ed8bf028aff456df3ebfeed77fd6
Alan Huang <a.huang232@gmail.com>
2022-10-15 12:37:06 -0700

    Add styling and javascript for login / signup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const userRoutes = require('./userRoutes');
 // const categoryRoutes = require('./categoryRoutes');
 // const recordRoutes = require('./recordRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 // router.use('/category', categoryRoutes);
 // router.use('/record', recordRoutes);
 
