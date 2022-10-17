95a08bab599263e99f05456d3c8e2b21b44474fe
Lydia Kim <lydiakim47@gmail.com>
2022-10-16 17:02:07 -0700

    record.handlebars and delete-record.js added and updated

---
+++ public/js/delete-record.js
@@ -0,0 +1,25 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split("/") [
+        window.location.toString().split("/").legnth -1
+    ];
+
+    const response = await fetch (`/api/record/${id}`, {
+        method: "DELETE",
+        body: JSON.stringify({
+            record_id: id
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        alert("Unable to delete record!");
+    }
+};
+
+document.querySelector(".delete-record-btn").addEventListener("click", deleteFormHandler);
