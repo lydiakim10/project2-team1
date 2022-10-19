29fa0fe341bdc3a8bcd031b34721addea818c753
dasunnie <shimdasun25@gmail.com>
2022-10-18 20:40:03 -0700

    category feature applied and expense js

---
+++ public/js/add-expense.js
@@ -0,0 +1,27 @@
+const addExpenseForm = async (event) => {
+    event.preventDefault();
+
+    const category = document.querySelector('#category-select').value.trim();
+    const merchant = document.querySelector('#mercha-input').value.trim();
+    const date = document.querySelector('#date-input').value.trim();
+    const amount = document.querySelector('#amount-input').value.trim();
+
+    if (category && merchant && date && amount) {
+        const response = await fetch(`/api/expense`, {
+          method: 'POST',
+          body: JSON.stringify({
+              month,
+              budget,
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert(response.statusText);
+        }
+      }
+  };
+  
+    document.querySelector('.addExpenseBtn').addEventListener('click', addExpense);
